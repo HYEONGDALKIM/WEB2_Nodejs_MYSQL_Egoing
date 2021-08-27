1d83afd98d3e823dbed5f268b8222253e3317562
HYEONGDALKIM <guddak@kakao.com>
2021-08-27 20:47:04 +0900

    nodejs

--- nodejs/mysql.js
+++ nodejs/mysql.js
@@ -1,16 +1,18 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
-  database : 'my_db'
+  user     : 'root',
+  password : '134679',
+  database : 'opentutorials'
 });
  
 connection.connect();
  
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+connection.query('SELECT * FROM topic', function (error, results, fields) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
 });
  
 connection.end();
